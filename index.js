0e0ed49f18160c5f33f60f61f7af8d7f4de36f50
morthos <pathshan61@gmail.com>
2018-11-13 15:03:00 -0500

    .

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express")
+const port = 3000
+const app = express()
+
+const globalUpdates = []
+let sendBackNumber = 0;
+app.use(express.static('public'))
+app.use(express.json())
+
+// Fill in your request handlers here
+app.post('/updates', (req, res) => {
+
+    if (req.body.clientUpdates.length) {
+        req.body.clientUpdates.forEach(element => {
+            globalUpdates.push(element)
+
+
+        });
+    }
+    if (globalUpdates.length > req.body.clientUpdateCount) {
+        sendBackNumber = globalUpdates.length - req.body.clientUpdateCount
+        console.log('global updates.length', globalUpdates.length)
+        console.log('client updates', req.body.clientUpdateCount)
+
+    }
+    let sendBackArray = globalUpdates.slice(sendBackNumber)
+    console.log(globalUpdates)
+    if (req.body.clientUpdateCount === 0) {
+        res.send({ globalUpdates: globalUpdates });
+    } else if (req.body.clientUpdateCount > 0) {
+        res.send({ globalUpdates: sendBackArray });
+    }
+
+})
+
+
+
+app.listen(port)
---
+++ public/polling.js
@@ -0,0 +1,35 @@
+// Add logic to this script to poll server every second for updated pixels.
+let clientUpdateCount = 0;
+
+function sendUpdates() {
+
+    const postReq = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "clientUpdates": bitmap.updates,
+            "clientUpdateCount": clientUpdateCount
+        })
+    }
+
+    fetch('/updates', postReq)
+        .then(response => response.json())
+        .then(data => {
+            clientUpdateCount += data.globalUpdates.length;
+            console.log();
+            data.globalUpdates.forEach(element => {
+                let row = element[0];
+                let col = element[1];
+                let color = element[2];
+                bitmap.setColor(row, col, color)
+            });
+
+
+        })
+    bitmap.updates = [];
+
+    setTimeout(sendUpdates, 3000)
+}
+sendUpdates()
